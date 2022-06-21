66bc982abda760aa340964dc317c2f5a0e195410
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-21 18:25:54 -0300

    add exercicio switch-case :rocket:

---
+++ 01-fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/05-ex-switch-case/01-ex-switch-case.js
@@ -0,0 +1,16 @@
+let estado = 'dsfadf'
+
+switch (estado) {
+    case 'aprovado':
+        console.log('aprovada');
+        break;
+    case 'lista':
+        console.log('lista');
+        break;
+    case 'reaprovado':
+        console.log('aprovada');
+        break;
+
+    default: console.log('nao se aplica')
+        break;
+}
