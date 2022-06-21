df3ce8ed8a11292ec7dbaa4361601cc4a0914678
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-21 15:40:01 -0300

    add exercicio aula tipos primitivos fixar

---
+++ 01-fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/02-ex-tipos-primitivos/01-ex-fixar-tipos-primitivos.js
@@ -0,0 +1,18 @@
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId, typeof isEnrolled,typeof patientInfo,typeof patientEmail);
+// console.log(typeof patientAge);
+// console.log(typeof patientId);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2*(base + height);
+console.log(perimeter)
