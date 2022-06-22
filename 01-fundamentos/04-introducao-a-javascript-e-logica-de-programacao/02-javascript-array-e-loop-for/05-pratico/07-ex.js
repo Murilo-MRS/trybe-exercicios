8dd1207ce164073794919db752ffaf532428128c
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-22 19:56:21 -0300

    add exercicio 7 pratico :rocket:

---
+++ 01-fundamentos/04-introducao-a-javascript-e-logica-de-programacao/02-javascript-array-e-loop-for/05-pratico/07-ex.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let menorNum = numbers[0];
+
+for (let index = 0; index < numbers.length; index++) {
+    if (menorNum > numbers[index]) {
+        menorNum = numbers[index];
+    }
+}
+
+console.log(menorNum);
+// usuaria Math.min
