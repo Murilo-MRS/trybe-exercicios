b0a52aae34ad9be7af95412192e3a662022e2b7f
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-24 19:02:59 -0300

    add exercicio 1 e 2 formatacao

--- 01-fundamentos/04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/02--ex.js
+++ 01-fundamentos/04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/02--ex.js
@@ -1,9 +1,9 @@
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-  
-info['recorrente'] = 'Sim';
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
 
-  console.log(info);
+info["recorrente"] = "Sim";
+
+console.log(info);
