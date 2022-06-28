e25aef0a56e15bf5640c856f7a88028058fb2b85
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-28 18:57:20 -0300

    add ex 2 e 3 pratico

--- 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/01-javascript-DOM-e-seletores/03-ex-pratico/script.js
+++ 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/01-javascript-DOM-e-seletores/03-ex-pratico/script.js
@@ -1,5 +1,17 @@
 function mudaTexto() {
-    let descricao = document.querySelectorAll('.center-content p')
+    let descricao = document.querySelectorAll('.center-content p');
     descricao[1].innerHTML = 'josnei xablau';
 }
 mudaTexto(); 
+
+function mudaCorMain() {
+    let mudaCor = document.getElementsByClassName('main-content');
+    mudaCor[0].style.backgroundColor = "rgb(76,164,109)";
+} 
+mudaCorMain();
+
+function mudaCorCenter() {
+    let mudaCor = document.getElementsByClassName('center-content');
+    mudaCor[0].style.backgroundColor = "rgb(255,255,255)";
+} 
+mudaCorCenter();
