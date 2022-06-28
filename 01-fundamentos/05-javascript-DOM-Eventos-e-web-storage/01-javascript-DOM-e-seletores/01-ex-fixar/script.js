4464e826e101c792476eb74d5f5cac09ecc4f337
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-28 16:02:03 -0300

    add exercicio 2 de fixar bloco 5.1

---
+++ 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/01-javascript-DOM-e-seletores/01-ex-fixar/script.js
@@ -0,0 +1,15 @@
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color = "#ff00ff";
+let titulo = document.getElementById("page-title");
+titulo.innerHTML = "Avengers";
+let secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.innerText =
+  "Et amet reprehenderit dolore adipisicing voluptate. Lorem sint do et anim et. Est quis dolore sunt amet eiusmod excepteur duis adipisicing.";
+let subTitle = document.getElementById("subtitle");
+subTitle.innerHTML = "Lorem ipsum";
+let escrita = document.getElementsByClassName("escrita");
+escrita[0].style.fontSize = "1.5rem";
+escrita[0].style.color = "green";
+
+let subTitulo = document.getElementsByTagName("h4")[0];
+subTitulo.style.backgroundColor = "orange";
