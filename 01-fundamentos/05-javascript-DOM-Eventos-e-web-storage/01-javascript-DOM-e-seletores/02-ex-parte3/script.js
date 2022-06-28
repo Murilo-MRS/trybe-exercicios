0988a4be4ddc78963d4d27ad41a7b19140167267
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-28 18:35:09 -0300

    add exercicio parte 3

---
+++ 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/01-javascript-DOM-e-seletores/02-ex-parte3/script.js
@@ -0,0 +1,25 @@
+let header = document.getElementById('container');
+header.style.backgroundColor = '#f3f3f3';
+
+let headerContainer = document.getElementById('header-container');
+headerContainer.style.backgroundColor = '#00b069';
+
+let emergencyTasks = document.querySelectorAll('.emergency-tasks div h3');
+emergencyTasks[0].style.backgroundColor = '#a500f3';
+emergencyTasks[1].style.backgroundColor = '#a500f3';
+
+let emergencyTasksSection = document.querySelectorAll('.emergency-tasks');
+emergencyTasksSection[0].style.backgroundColor = '#ff9f84';
+emergencyTasksSection[0].style.backgroundColor = '#ff9f84';
+
+let noEmergencyTasks = document.querySelectorAll('.no-emergency-tasks div h3');
+noEmergencyTasks[0].style.backgroundColor = '#232525';
+noEmergencyTasks[1].style.backgroundColor = '#232525';
+
+let noEmergencyTasksSection = document.querySelectorAll('.no-emergency-tasks');
+noEmergencyTasksSection[0].style.backgroundColor = '#f9db5e';
+noEmergencyTasksSection[0].style.backgroundColor = '#f9db5e';
+
+let footerContainer = document.getElementById('footer-container');
+footerContainer.style.backgroundColor = '#003533';
+
