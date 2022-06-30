2eb03c67509c0178c52838582de7e8a84d8d394b
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-30 09:01:21 -0300

    add duvidas parte 3 remover elementos :rocket:

--- 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/02-javascript-trabalhando-com-elementos/04-ex-parte3/script.js
+++ 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/02-javascript-trabalhando-com-elementos/04-ex-parte3/script.js
@@ -1,30 +1,31 @@
-  let acessaPai = document.getElementById("pai");
 
-  
-  for (let index = acessaPai.childNodes.length - 1; index >= 0; index -= 1) {
-    let element = acessaPai.childNodes[index];
-    if (element.id !== 'elementoOndeVoceEsta') {
-      acessaPai.removeChild(element);
-    }
+let acessaPai = document.getElementById("pai");
+
+
+for (let index = acessaPai.childNodes.length - 1; index >= 0; index -= 1) {
+  let element = acessaPai.childNodes[index];
+  if (element.id !== 'elementoOndeVoceEsta') {
+    acessaPai.removeChild(element);
   }
-  console.log(acessaPai.childNodes);
+}
+console.log(acessaPai.childNodes);
 
-  // console.log(elementoOnde);
-  
-  // let elementoOnde = document.getElementById('elementoOndeVoceEsta');
-  // elementoOnde.removeChild('segundoEUltimoFilhoDoFilho')
-  // console.log(elementoOnde);
-  // elementoOnde.removeChild('segundoEUltimoFilhoDoFilho');
-  
+let elementoOnde = document.getElementById('elementoOndeVoceEsta');
+elementoOnde.removeChild('segundoEUltimoFilhoDoFilho')
+console.log(elementoOnde);
 
-  // const pai = document.getElementById('pai');
+// console.log(elementoOnde);
+// elementoOnde.removeChild('segundoEUltimoFilhoDoFilho');
 
-  // for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
-  //   const currentChildren = pai.childNodes[index];
-  //   if (currentChildren.id !== 'elementoOndeVoceEsta') {
-  //     currentChildren.remove();
-  //   }
-  // }
 
-  const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
-  segundoEUltimoFilhoDoFilho.remove();
+// const pai = document.getElementById('pai');
+
+// for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+//   const currentChildren = pai.childNodes[index];
+//   if (currentChildren.id !== 'elementoOndeVoceEsta') {
+//     currentChildren.remove();
+//   }
+// }
+
+// const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+// segundoEUltimoFilhoDoFilho.remove();
