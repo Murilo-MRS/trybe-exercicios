8d116b6a0339fcfdf95df544ce1079b00d62173c
Murilo-MRS <murilosantana1993@gmail.com>
2022-06-30 11:45:15 -0300

    add exercicio pratico 8

--- 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/02-javascript-trabalhando-com-elementos/05-ex-pratico/script.js
+++ 01-fundamentos/05-javascript-DOM-Eventos-e-web-storage/02-javascript-trabalhando-com-elementos/05-ex-pratico/script.js
@@ -6,48 +6,61 @@ function exercicio1() {
 exercicio1();
 
 function exercicio2() {
-    let criaMain = document.createElement("main");
-    criaMain.className = "main-content";
-    document.body.appendChild(criaMain);
+  let criaMain = document.createElement("main");
+  criaMain.className = "main-content";
+  document.body.appendChild(criaMain);
 }
 exercicio2();
 
-let mainPai = document.querySelector('main');
+let mainPai = document.querySelector("main");
 function exercicio3() {
-    let criaSection = document.createElement('section');
-    criaSection.className = 'center-content';
-    mainPai.appendChild(criaSection);
+  let criaSection = document.createElement("section");
+  criaSection.className = "center-content";
+  mainPai.appendChild(criaSection);
 }
 exercicio3();
 
 function exercicio4() {
-    let sectionPai = document.querySelector('section');
-    let criaP = document.createElement('p');
-    criaP.innerText = 'TEXTO VEM AQUI';
-    sectionPai.appendChild(criaP);
+  let sectionPai = document.querySelector("section");
+  let criaP = document.createElement("p");
+  criaP.innerText = "TEXTO VEM AQUI";
+  sectionPai.appendChild(criaP);
 }
 exercicio4();
 
 function exercicio5() {
-    let criaSection = document.createElement('section');
-    criaSection.className = 'left-content';
-    mainPai.appendChild(criaSection);
+  let criaSection = document.createElement("section");
+  criaSection.className = "left-content";
+  mainPai.appendChild(criaSection);
 }
 exercicio5();
 
 function exercicio6() {
-    let criaSection = document.createElement('section');
-    criaSection.className = 'right-content';
-    mainPai.appendChild(criaSection);
+  let criaSection = document.createElement("section");
+  criaSection.className = "right-content";
+  mainPai.appendChild(criaSection);
 }
 exercicio6();
 
 function exercicio7() {
-    let sectionLeft = document.querySelector('.left-content');
-    let criaImg = document.createElement('img');
-    criaImg.src = 'https://picsum.photos/200';
-    criaImg.className = 'small-image';
-    sectionLeft.appendChild(criaImg);
+  let sectionLeft = document.querySelector(".left-content");
+  let criaImg = document.createElement("img");
+  criaImg.src = "https://picsum.photos/200";
+  criaImg.className = "small-image";
+  sectionLeft.appendChild(criaImg);
 }
 exercicio7();
 
+function exercicio8() {
+  let sectionRight = document.querySelector(".right-content");
+  let criaUl = document.createElement("ul");
+  sectionRight.appendChild(criaUl);
+  let listaLi = ["um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez",];
+
+  for (let index = 0; index < listaLi.length; index++) {
+    let criaLi = document.createElement("li");
+    criaLi.innerText = listaLi[index];
+    criaUl.appendChild(criaLi);
+  }
+}
+exercicio8();
