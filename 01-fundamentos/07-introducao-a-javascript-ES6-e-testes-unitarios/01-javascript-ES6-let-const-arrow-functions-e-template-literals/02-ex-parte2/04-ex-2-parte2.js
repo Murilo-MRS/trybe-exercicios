4bda19f0665f26557854c8da71d404580a476607
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-14 19:40:22 -0300

    ex 02 parte 2

---
+++ 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/01-javascript-ES6-let-const-arrow-functions-e-template-literals/02-ex-parte2/04-ex-2-parte2.js
@@ -0,0 +1,15 @@
+const longestWord = (frase) => {
+    const arrayPalavra = frase.split(' ');
+    let aux = 0;
+    let resultado = '';
+    for (let index = 0; index < arrayPalavra.length; index += 1) {
+        const element = arrayPalavra[index];
+        if (element.length > aux)  {
+            aux = element.length
+            resultado = arrayPalavra[index];
+        }
+    }
+    return `${resultado} e com tamanho ${aux}`;
+}
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+ // retorna 'aconteceu'
