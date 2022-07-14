a11478d352535da2d90fb67d0f4c2df9e4cc537a
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-14 18:17:41 -0300

    ex 01 parte 1

--- 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/01-javascript-ES6-let-const-arrow-functions-e-template-literals/01-ex-parte1.js
+++ 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/01-javascript-ES6-let-const-arrow-functions-e-template-literals/01-ex-parte1.js
@@ -1,13 +1,7 @@
-function testingScope(escopo) {
-    if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+const testingScope = (escopo) => {
+  let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+  let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+  (escopo === true) ? console.log(`${ifScope} ótimo, fui utilizada no escopo !`) : console.log(elseScope)
   }
 
   testingScope(true);
