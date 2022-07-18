a7fa9b34f7ac70dad2d57a8582fda5274ac61cde
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-18 12:17:07 -0300

    exercio 3 a 6 da parte 3 pratico

--- 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/02-javascript-ES6-fluxo-de-excecao-e-objetos/04-ex-parte3/script.js
+++ 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/02-javascript-ES6-fluxo-de-excecao-e-objetos/04-ex-parte3/script.js
@@ -17,7 +17,7 @@ const lesson3 = {
   professor: "Maria Clara",
   turno: "noite",
 };
-
+// exercicio 1 
 const adicionaTurno = (obj, key, value) => {
     obj[key] = value;
     return obj;
@@ -25,5 +25,37 @@ const adicionaTurno = (obj, key, value) => {
 adicionaTurno(lesson2, 'turno', 'noite');
 console.log(lesson2);
 
+// exercicio 2 
 const listaKeys = obj => Object.keys(obj);
 console.log(listaKeys(lesson2));
+
+// exercicio 3
+const tamanhoObj = obj => Object.keys(obj).length;
+console.log(tamanhoObj(lesson2));
+
+// exercicio 4
+const listaValues = obj => Object.values(obj);
+console.log(listaValues(lesson2));
+
+// exercicio 5
+const lessons = {lesson1, lesson2, lesson3};
+const allLessons = Object.assign({}, lessons);
+console.log(allLessons);
+
+// exercicio 6
+const retornaTotalDeAlunos = obj => {
+    let totalAlunos = 0;
+    // obj.lesson1.numeroEstudantes + obj.lesson2.numeroEstudantes + obj.lesson3.numeroEstudantes;
+    const lesson = Object.keys(obj);
+    for (const key in lesson) {
+        totalAlunos += obj[lesson[key]].numeroEstudantes;
+    }
+    return totalAlunos;
+};
+console.log(retornaTotalDeAlunos(allLessons));
+// exercicio 7
+
+
+// exercicio 8
+
+
