18ed294a1e5e436e4a7b988c4d80e39767337b17
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-21 15:48:23 -0300

    exercicio fixar HOFS

---
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/01-javascript-ES6-introducao-a-high-order-functions/02-ex-fixar.js
@@ -0,0 +1,7 @@
+const wakeUp = () => console.log('Acordando!!!');;
+const coffeTime = () => console.log('Bora tomar café!!');;
+const sleep = () => console.log('Partiu dormir!!');;
+
+const doingThings = (func) => func();
+
+doingThings(coffeTime)
