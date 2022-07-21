71ef006b4f4ab8aab39c4eae30907f0873ddb77c
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-21 19:15:46 -0300

    ex 2 pratica refeito

--- 01-fundamentos/08-higher-order-functions-do-javascript-ES6/01-javascript-ES6-introducao-a-high-order-functions/02-ex-pratica/02-ex-pratica.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/01-javascript-ES6-introducao-a-high-order-functions/02-ex-pratica/02-ex-pratica.js
@@ -4,11 +4,19 @@ const sorteio = () => {
 }
 
 const hof = (aposta, callback) => {
-    let retorno = "Tente novamente"
-    if (aposta === callback()) {
-        retorno = "Parabéns você ganhou"
-    };
-    return console.log(retorno);
+    return aposta === callback() ? 'Parabéns você ganhou' : 'Tente de novo!';
 };
 
-console.log(hof(2, sorteio));
+console.log(hof(4, sorteio));
+
+
+////////////
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
