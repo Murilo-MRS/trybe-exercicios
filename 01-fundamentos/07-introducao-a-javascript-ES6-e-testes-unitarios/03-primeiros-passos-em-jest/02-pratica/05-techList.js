1e3376cfe76db52b34fc00add40a994876466ee0
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-22 11:25:37 -0300

    ex 5 pratica testes

--- 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/03-primeiros-passos-em-jest/02-pratica/05-techList.js
+++ 01-fundamentos/07-introducao-a-javascript-ES6-e-testes-unitarios/03-primeiros-passos-em-jest/02-pratica/05-techList.js
@@ -1,8 +1,10 @@
 const techList = (arr, name) => {
   //   if (arrayTechnologies.length === 0) return "Vazio!";
+  if (arr.length === 0) {
+    return 'Vazio!'
+  }
   const oredenadoArray = arr.sort();
   const array = [];
-
   for (let index = 0; index < oredenadoArray.length; index += 1) {
     array.push({
       tech: oredenadoArray[index],
