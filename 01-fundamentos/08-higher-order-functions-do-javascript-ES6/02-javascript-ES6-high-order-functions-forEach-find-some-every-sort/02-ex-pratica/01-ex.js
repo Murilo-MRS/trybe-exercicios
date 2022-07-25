da03433f0fb1b83ec58aa0d8176edbf8da42cf3e
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-25 10:23:24 -0300

    ex 2 a 4 pratica

--- 01-fundamentos/08-higher-order-functions-do-javascript-ES6/02-javascript-ES6-high-order-functions-forEach-find-some-every-sort/02-ex-pratica/01-ex.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/02-javascript-ES6-high-order-functions-forEach-find-some-every-sort/02-ex-pratica/01-ex.js
@@ -1,79 +1,103 @@
 const books = [
-    {
-      id: 1,
-      name: 'As Crônicas de Gelo e Fogo',
-      genre: 'Fantasia',
-      author: {
-        name: 'George R. R. Martin',
-        birthYear: 1948,
-      },
-      releaseYear: 1991,
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
     },
-    {
-      id: 2,
-      name: 'O Senhor dos Anéis',
-      genre: 'Fantasia',
-      author: {
-        name: 'J. R. R. Tolkien',
-        birthYear: 1892,
-      },
-      releaseYear: 1954,
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
     },
-    {
-      id: 3,
-      name: 'Fundação',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Isaac Asimov',
-        birthYear: 1920,
-      },
-      releaseYear: 1951,
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
     },
-    {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Frank Herbert',
-        birthYear: 1920,
-      },
-      releaseYear: 1965,
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
     },
-    {
-      id: 5,
-      name: 'A Coisa',
-      genre: 'Terror',
-      author: {
-        name: 'Stephen King',
-        birthYear: 1947,
-      },
-      releaseYear: 1986,
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
     },
-    {
-      id: 6,
-      name: 'O Chamado de Cthulhu',
-      genre: 'Terror',
-      author: {
-        name: 'H. P. Lovecraft',
-        birthYear: 1890,
-      },
-      releaseYear: 1928,
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
     },
-  ];
-  
-  // Adicione o código do exercício aqui:
+    releaseYear: 1928,
+  },
+];
 
-  ///// exercicio 1
-  const authorBornIn1947 = (born) => {
-    return books.find((element) => element.author.birthYear === born).author.name
-  }
-  console.log(authorBornIn1947(1947)); // LEMBRAR SEMPPRE DO RETURN
+// Adicione o código do exercício aqui:
+
+///// exercicio 1
+const authorBornIn1947 = (born) => {
+  return books.find((element) => element.author.birthYear === born).author.name;
+};
+// LEMBRAR SEMPPRE DO RETURN
 ///// exercicio 2
 function smallerName() {
-    let nameBook;
-    // escreva aqui o seu código
-    
-    // Variável nameBook que receberá o valor do menor nome;
-    return nameBook;
+  let nameBook;
+  // escreva aqui o seu código
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
 }
 ///// exercicio 3
+function getNamedBook(characters) {
+    // escreva seu código aqui
+    let nameBook = "";
+    books.find((element) => {
+        if (element.name.length === characters) nameBook = element.name;
+    });
+    return nameBook;
+}
+
+///// exercicio 4
+function booksOrderedByReleaseYearDesc() {
+    // escreva aqui seu código
+    books.sort((a, b) => a.releaseYear < b.releaseYear ? 1 : -1)
+    
+}
+
+///// exercicio 5
+function everyoneWasBornOnSecXX() {
+    // escreva seu código aqui
+  }
