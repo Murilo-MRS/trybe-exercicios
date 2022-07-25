22b0a78563441424d8c64168aa05be24c4a183e1
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-25 15:51:48 -0300

    exercicio 2 map fixar comentado

--- 01-fundamentos/08-higher-order-functions-do-javascript-ES6/03-javascript-ES6-high-order-functions-map-filters/01-ex-fixar/02-ex-map.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/03-javascript-ES6-high-order-functions-map-filters/01-ex-fixar/02-ex-map.js
@@ -78,3 +78,12 @@ const allNameStudents = estudantes
   .map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
 
 console.log(allNameStudents);
+
+const reportStatus = (name, students) => {
+    const studentInfo = students.find((student) => student.nome === name);
+    return studentInfo.materias.map((materia) => (
+      `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+    ));
+  };
+/*   O código teve uma redução drástica no número de linhas! Primeiro, fizemos um find para buscar e retornar os dados do estudante. O objeto foi retornado e salvo na variável studentsInfo, depois o map foi usado para percorrer as matérias do objeto retornado e salvar o que se queria em um array da forma desejada. */
+  console.log(reportStatus('Natalia', estudantes));
