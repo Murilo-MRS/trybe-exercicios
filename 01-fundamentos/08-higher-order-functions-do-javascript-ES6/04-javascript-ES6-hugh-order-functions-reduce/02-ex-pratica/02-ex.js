54a5dd1e949c1bb4d88685ca29f19467ae873204
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-26 21:46:39 -0300

    exercicio 3 e 4 pratica

--- 01-fundamentos/08-higher-order-functions-do-javascript-ES6/04-javascript-ES6-hugh-order-functions-reduce/02-ex-pratica/02-ex.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/04-javascript-ES6-hugh-order-functions-reduce/02-ex-pratica/02-ex.js
@@ -1,8 +1,32 @@
 const books = require("./00-ex-dados");
-
+/*--------- EX 2-------------- */
 const reduceNames = (arr) => {
   return arr.reduce((acc, curr, index, array) => {
-    return (index === array.length -1) ? `${acc} ${curr.author.name}.` : `${acc} ${curr.author.name},`
-  },'');
+    return index === array.length - 1
+      ? `${acc} ${curr.author.name}.`
+      : `${acc} ${curr.author.name},`;
+  }, "");
 };
-console.log(reduceNames(books));
+// console.log(reduceNames(books));
+/*--------- EX 3-------------- */
+const averageAge = (arr) => {
+  return arr
+    .map((element) => element.releaseYear - element.author.birthYear)
+    .reduce((acc, curr) => {
+      return (acc + curr);
+    }, 0) / arr.length;
+};
+console.log(averageAge(books));
+/*--------- EX 4-------------- */
+const longestNamedBook = (arr) => {
+    return arr.reduce((acc, curr) => {
+        return (acc.name.length > curr.name.length) ? acc : curr;
+    })
+};
+console.log(longestNamedBook(books));
+/*--------- EX 5-------------- */
+const containsA = (arr) => {
+    
+};
+/*--------- EX 6-------------- */
+const studentAverage = () => {};
