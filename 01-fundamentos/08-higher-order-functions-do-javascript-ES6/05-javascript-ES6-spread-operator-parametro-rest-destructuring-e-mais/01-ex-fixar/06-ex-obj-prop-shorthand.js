129d332f9ceadf11a403be22c23fb36a0c114365
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-27 21:02:28 -0300

    exercio 1 fixar obj shorthand

--- 01-fundamentos/08-higher-order-functions-do-javascript-ES6/05-javascript-ES6-spread-operator-parametro-rest-destructuring-e-mais/01-ex-fixar/06-ex-obj-prop-shorthand.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/05-javascript-ES6-spread-operator-parametro-rest-destructuring-e-mais/01-ex-fixar/06-ex-obj-prop-shorthand.js
@@ -1,5 +1,6 @@
 const getPosition = (latitude, longitude) => ({
-    latitude: latitude,
-    longitude: longitude});
+    latitude,
+    longitude
+  });
   
   console.log(getPosition(-19.8157, -43.9542));
