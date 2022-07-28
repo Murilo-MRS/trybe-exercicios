c0ab77745d576b3da5411d2ab389341f95f6f65c
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-27 21:00:48 -0300

    exercio 1 fixar default destruct

--- 01-fundamentos/08-higher-order-functions-do-javascript-ES6/05-javascript-ES6-spread-operator-parametro-rest-destructuring-e-mais/01-ex-fixar/05-ex-default-destruct.js
+++ 01-fundamentos/08-higher-order-functions-do-javascript-ES6/05-javascript-ES6-spread-operator-parametro-rest-destructuring-e-mais/01-ex-fixar/05-ex-default-destruct.js
@@ -6,19 +6,20 @@
 
 // const { nationality = "Brazilian" } = person;
 // console.log(nationality); // Brazilian
-
 const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
 const person = {
   firstName: 'João',
   lastName: 'Jr II',
 };
-
+const {firstName, lastName, nationality = 'Brazilian'} = person;
+person.nationality = nationality;
 const otherPerson = {
   firstName: 'Ivan',
   lastName: 'Ivanovich',
   nationality: 'Russian',
 };
+
 
 console.log(getNationality(otherPerson)); // Ivan is Russian
 console.log(getNationality(person));
