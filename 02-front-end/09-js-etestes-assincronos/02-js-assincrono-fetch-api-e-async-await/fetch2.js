d801cde7c84a6540bf9c54df24b183066ce8013e
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-05 16:19:09 -0300

    exemplo fetch API e async e await

---
+++ 02-front-end/09-js-etestes-assincronos/02-js-assincrono-fetch-api-e-async-await/fetch2.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.value)
+    .catch((error) => `Algo deu errado :( \n${error}`);
+
+  console.log(result);
+};
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+
+const fetchJokes = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJokes();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
