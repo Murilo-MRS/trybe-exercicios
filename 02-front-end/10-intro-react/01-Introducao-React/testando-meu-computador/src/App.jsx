0b271027784453ca33a708b8a6736ace70a41dbd
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-16 18:10:18 -0300

    exercicio aulaaovivo e fixar

---
+++ 02-front-end/10-intro-react/01-Introducao-React/testando-meu-computador/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tribo 24 - A</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia necessitatibus dolorum, cumque laboriosam maiores numquam aperiam ut facilis ex quaerat? Magnam amet necessitatibus voluptas totam aliquam, porro at ullam culpa?</p>
+    </div>
+  );
+}
+
+export default App;
