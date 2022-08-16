be7db313cddeea9a1adf4e0d0abc7af7ff5462aa
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-16 19:33:15 -0300

    exercicio 2 e 3 fixar

---
+++ 02-front-end/10-intro-react/01-Hello-world-no-React/01-ex-fixar/src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Murilo</h1>
+                <p>Engenheiro de formacao, em transicao de carreira</p>
+            </div>
+        );
+    }
+}
+
+export default About;
