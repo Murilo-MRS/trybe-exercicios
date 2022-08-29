6b5f5ee645af15a73ed0a1cc3151f7179b9cb303
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-29 14:41:57 -0300

    ex-02 fixar component user

---
+++ 02-front-end/12-ciclo-de-vida-de-componentes-react-router/01-Ciclo-de-vida-de-componentes/02-ex-fixar/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import User from './components/User';
+
+class App extends Component {
+  state = {
+    results: undefined,
+    loading: true,
+  }
+  
+  fetchUser =  () => {
+    this.setState({
+      loading: true,
+    },async () => {
+      const requestUser = await fetch('https://api.randomuser.me/');
+      const requestArray = await requestUser.json();
+      this.setState({
+        results: requestArray[0],
+        loading: false,
+      })
+    })
+  }
+
+  componentDidMount () {
+    this.fetchUser();
+  }
+
+  render () {
+    const { loading, results } = this.state;
+    const loadingElement  = 'Carregando...';
+
+    return (
+      <div>
+        { loading
+            ? loadingElement
+            : <User results={results}/>
+        }
+      </div>
+    );
+  }
+
+};
+
+export default App;
---
+++ 02-front-end/12-ciclo-de-vida-de-componentes-react-router/01-Ciclo-de-vida-de-componentes/02-ex-fixar/src/components/User.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+    render() {
+        const { results } = this.props
+
+        return (
+            <div>
+                <p></p>
+            </div>
+        )
+    }
+};
+
+export default User;
