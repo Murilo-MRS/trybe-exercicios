90313ef2375ec938bba8f86c5de94f2a72a7bd00
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-29 21:56:51 -0300

    ex-02 func invalida should

--- 02-front-end/12-ciclo-de-vida-de-componentes-react-router/01-Ciclo-de-vida-de-componentes/02-ex-fixar/src/components/Card.jsx
+++ 02-front-end/12-ciclo-de-vida-de-componentes-react-router/01-Ciclo-de-vida-de-componentes/02-ex-fixar/src/components/Card.jsx
@@ -1,19 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Card extends Component {
-    render() {
-        const { results } = this.props
-        const { name: {title, first, last,}, dob: {age}, email, picture: { thumbnail } } = results
+  render() {
+    const { results } = this.props;
+    const {
+      name: { title, first, last },
+      dob: { age },
+      email,
+      picture: { thumbnail },
+    } = results;
 
-        return ( 
-            <div>
-                <p>{title} {first} {last}</p>
-                <p>{email}</p>
-                <p>{age} years old</p>
-                <img src={thumbnail} alt={first}/>
-            </div>
-        )
-    }
-};
+    return ( age < 50 &&
+      <div>
+        <p>
+          {title} {first} {last}
+        </p>
+        <p>{email}</p>
+        <p>{age} years old</p>
+        <img src={thumbnail} alt={first} />
+      </div>
+    );
+  }
+}
 
 export default Card;
--- 02-front-end/12-ciclo-de-vida-de-componentes-react-router/01-Ciclo-de-vida-de-componentes/02-ex-fixar/src/components/User.jsx
+++ 02-front-end/12-ciclo-de-vida-de-componentes-react-router/01-Ciclo-de-vida-de-componentes/02-ex-fixar/src/components/User.jsx
@@ -24,7 +24,7 @@ class User extends Component {
       componentDidMount () {
         this.fetchUser();
       }
-    
+
       render () {
         const { loading, results } = this.state;
         
