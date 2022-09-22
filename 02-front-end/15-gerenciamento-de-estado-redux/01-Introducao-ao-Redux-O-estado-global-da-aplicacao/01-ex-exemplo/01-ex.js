25d838f3884795c903b95aab084924a3f76c55d6
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-22 20:01:22 -0300

    [Dev] [config] exercio fixar 01

---
+++ 02-front-end/15-gerenciamento-de-estado-redux/01-Introducao-ao-Redux-O-estado-global-da-aplicacao/01-ex-exemplo/01-ex.js
@@ -0,0 +1,33 @@
+const Redux = require("redux");
+// type dentro de dispatch como passamos param p/ essa funcao usa-se arrow func
+const fazerLogin = () => ({
+    type: "LOGIN",
+    email: 'teste@teste',
+  });
+
+const ESTADO_INICIAL = {
+  login: false,
+  email: "",
+};
+
+//================REDUCER=============///
+const reducer = (state = ESTADO_INICIAL, action) => {
+    switch (action.type) {
+        case "LOGIN":
+            return {
+                ...state,
+                login: !state.login,
+                email: action.email,
+            };
+        default:
+            return state;
+    }
+};
+
+//================STORE=============///
+const store = Redux.createStore(reducer);
+
+console.log(store.getState());
+//================ACTIONS=============///
+store.dispatch(fazerLogin())
+console.log(store.getState());
