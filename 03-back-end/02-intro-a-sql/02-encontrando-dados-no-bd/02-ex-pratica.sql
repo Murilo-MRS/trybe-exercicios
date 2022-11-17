08cdd879f631d7de13ec73174f269e729a1b07ba
Murilo-MRS <murilosantana1993@gmail.com>
2022-11-17 18:54:17 -0300

    feat: item 7

--- 03-back-end/02-intro-a-sql/02-encontrando-dados-no-bd/02-ex-pratica.sql
+++ 03-back-end/02-intro-a-sql/02-encontrando-dados-no-bd/02-ex-pratica.sql
@@ -1,3 +1,7 @@
 USE Scientists;
 SELECT 'This is SQL Exercise, Practice and Solution';
 SELECT 1, 2, 3;
+SELECT 10 + 15;
+SELECT (3 * 4) + 12;
+SELECT * FROM Scientists;
+SELECT Name AS 'Nome do Projeto', Hours AS 'Tempo de Trabalho' FROM Projects;
