45acbf8e676dd623036f191cbb020e3d9732f9d4
Murilo-MRS <murilosantana1993@gmail.com>
2022-11-17 15:29:28 -0300

    feat: exemplo 2

--- 03-back-end/02-intro-a-sql/02-encontrando-dados-no-bd/01-ex-exemplo.sql
+++ 03-back-end/02-intro-a-sql/02-encontrando-dados-no-bd/01-ex-exemplo.sql
@@ -1,5 +1,5 @@
 SELECT 'Murilo';
-SELECT 'Murilo','Rodrigues Santana','Catalão','29';
-SELECT 'Murilo' AS nome, 'Rodrigues Santana' AS sobrenome, 'xablau' AS 'cidade natal', 29 AS idade;
+SELECT 'Murilo','xams xable','xablauland','29';
+SELECT 'Murilo' AS nome, 'xams xable' AS sobrenome, 'xablauland' AS 'cidade natal', 29 AS idade;
 SELECT 13 * 8;
 SELECT now() AS 'Data Atual';
