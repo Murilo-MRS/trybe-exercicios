658b17cfb02301e5bbcbc9ff55979545dbed4cf8
Murilo-MRS <murilosantana1993@gmail.com>
2022-11-17 15:28:03 -0300

    feat: exemplo de comandos

---
+++ 03-back-end/02-intro-a-sql/02-encontrando-dados-no-bd/01-ex-exemplo.sql
@@ -0,0 +1,5 @@
+SELECT 'Murilo';
+SELECT 'Murilo','Rodrigues Santana','Catalão','29';
+SELECT 'Murilo' AS nome, 'Rodrigues Santana' AS sobrenome, 'xablau' AS 'cidade natal', 29 AS idade;
+SELECT 13 * 8;
+SELECT now() AS 'Data Atual';
