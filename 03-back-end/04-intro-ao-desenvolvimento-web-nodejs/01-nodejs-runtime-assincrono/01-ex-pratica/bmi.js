1d904f1ef1793117d8ab33933c3162548c8b2d59
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-07 11:31:54 -0300

    feat::tada: ex 03 pratica

--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/01-nodejs-runtime-assincrono/01-ex-pratica/bmi.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/01-nodejs-runtime-assincrono/01-ex-pratica/bmi.js
@@ -1,6 +1,13 @@
+const readlineSync = require('readline-sync');
+
+const weight = readlineSync.question('what is your weight(kg)? ');
+const height = readlineSync.question('what is your height(cm)? ');
+
+
 const bmi = (weight, height) => {
   const heightInMeter = height / 100
-  return weight / (heightInMeter ** 2)
+  const result = (weight / (heightInMeter ** 2)).toFixed(2);
+  return result;
 };
 
-console.log(bmi(87, 177));
+console.log(`Your BMI is: `,bmi(weight, height));
