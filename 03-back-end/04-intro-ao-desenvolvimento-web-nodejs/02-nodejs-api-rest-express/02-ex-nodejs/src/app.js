5c26e5379592e1c8eaa76bbb5460b53e5d86e7d0
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-07 19:30:47 -0300

    fix: lint

--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/02-nodejs-api-rest-express/02-ex-nodejs/src/app.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/02-nodejs-api-rest-express/02-ex-nodejs/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.get('/', (_req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
