51656b15173134f71d10debfb42878d20a035dc4
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-08 10:35:48 -0300

    feat:tada:: ex 06 a 7 pratica

--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/02-nodejs-api-rest-express/02-ex-nodejs/src/app.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/02-nodejs-api-rest-express/02-ex-nodejs/src/app.js
@@ -40,4 +40,21 @@ app.get('/movies/:id', async (req, res) => {
   }
 });
 
+app.post('/movies/', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 module.exports = app;
