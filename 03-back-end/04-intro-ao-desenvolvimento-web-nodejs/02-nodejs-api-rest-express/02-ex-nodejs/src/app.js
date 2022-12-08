74e7361219a3653acf277eedd8b6626690d3319f
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-08 09:56:51 -0300

    feat:tada:: ex 04 a 05 pratica

--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/02-nodejs-api-rest-express/02-ex-nodejs/src/app.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/02-nodejs-api-rest-express/02-ex-nodejs/src/app.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 
+const moviesPath = path.resolve(__dirname, './movies.json');
 app.use(express.json());
 
-app.get('/', (_req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
+
+app.get('/movies', async (_req, res) => {
+  try {
+    const movies = await readFile();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const movies = await readFile();
+    const movieById = movies.find((e) => e.id === +id);
+    if (!movieById) {
+      res.status(404).send({ message: 'This id do not exist' });
+    }
+    res.status(200).json(movieById);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 module.exports = app;
