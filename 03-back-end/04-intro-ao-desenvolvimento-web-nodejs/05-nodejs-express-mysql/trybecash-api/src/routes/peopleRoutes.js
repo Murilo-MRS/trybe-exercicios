ce157df07c142a50fff4360f97195eb810c17a73
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-13 20:04:19 -0300

    feat: add rota people

---
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/05-nodejs-express-mysql/trybecash-api/src/routes/peopleRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const person = req.body;
+  res.status(201).json(person);
+});
+
+module.exports = router;
