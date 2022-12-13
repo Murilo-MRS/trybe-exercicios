14aad8895ec5fadd57aeee66e9dcbbf46a66ed59
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-13 16:22:14 -0300

    chore: refatorando log

--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/05-nodejs-express-mysql/trybecash-api/src/server.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/05-nodejs-express-mysql/trybecash-api/src/server.js
@@ -6,11 +6,4 @@ const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
-
-  // O código abaixo é para testarmos a comunicação com o MySQL
-  const [ result ] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
-
 });
