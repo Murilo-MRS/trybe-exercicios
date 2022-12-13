d8d53bafefe36d7e173d6ca3893ecd022a4b2a35
Murilo-MRS <murilosantana1993@gmail.com>
2022-12-13 12:22:28 -0300

    feat: ex 7

--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/04-nodejs-express-middlewares/01-ex-pratica/src/app.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/04-nodejs-express-middlewares/01-ex-pratica/src/app.js
@@ -1,26 +1,20 @@
 const express = require('express');
-const { validateName, validatePrice, validateDescription } = require('./middlewares/index');
+const { validateName,
+  validatePrice,
+  validateDescription,
+  validateCreatedAt } = require('./middlewares/index');
 
 const app = express();
 
 app.use(express.json());
 
-// const dataPath = path.resolve(__dirname,)
-
-// app.get('/teams', (req, res) => res.json(teams));
-
 app.get('/activities', (req, res) => res.status(200).json({ message: 'OK' }));
 
-app.post('/activities', validateName, validatePrice, validateDescription, (_req, res) => {
-  return res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
-});
-
-// app.put('/teams/:id', validateTeam, (req, res) => {
-
-// });
-
-// app.delete('/teams/:id', (req, res) => {
-
-// });
+app.post('/activities',
+  validateName,
+  validatePrice,
+  validateDescription,
+  validateCreatedAt,
+  (_req, res) => res.status(201).json({ message: 'Atividade cadastrada com sucesso!' }));
 
 module.exports = app;
--- 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/04-nodejs-express-middlewares/01-ex-pratica/src/middlewares/index.js
+++ 03-back-end/04-intro-ao-desenvolvimento-web-nodejs/04-nodejs-express-middlewares/01-ex-pratica/src/middlewares/index.js
@@ -38,8 +38,19 @@ const validateDescription = (req, res, next) => {
     || next();
 };
 
+const validateCreatedAt = (req, res, next) => {
+  const { createdAt } = req.body.description;
+  const isFormatDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+  if (!isFormatDate.test(createdAt)) {
+    return res.status(400).json({ message: 'O campo createdAt deve ter o formato dd/mm/aaaa' });
+  }
+  next();
+}; 
+
 module.exports = {
   validateName,
   validatePrice,
   validateDescription,
+  validateCreatedAt,
 };
