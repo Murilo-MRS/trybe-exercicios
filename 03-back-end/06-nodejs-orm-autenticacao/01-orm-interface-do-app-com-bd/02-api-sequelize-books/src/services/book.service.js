6d0636f96dc4594e57fd49179597fac8003dcdfb
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-25 21:29:14 -0300

    feat: service - books - create, read

--- 03-back-end/06-nodejs-orm-autenticacao/01-orm-interface-do-app-com-bd/02-api-sequelize-books/src/services/book.service.js
+++ 03-back-end/06-nodejs-orm-autenticacao/01-orm-interface-do-app-com-bd/02-api-sequelize-books/src/services/book.service.js
@@ -5,6 +5,22 @@ const getAll = async () => {
   return books
 };
 
+const getById = async (bookId) => {
+  const book = await Book.findByPk(bookId);
+
+  if (!book) throw { type: 'BOOK_NOT_FOUND', message: 'Book not found' };
+  
+  return { type: '', message: book };
+};
+
+const create = async (bookInfo) => {
+  const newBook = await Book.create(bookInfo);
+  console.log(newBook);
+  return { type: '', message: newBook };
+};
+
 module.exports = {
   getAll,
-}
+  getById,
+  create,
+};
