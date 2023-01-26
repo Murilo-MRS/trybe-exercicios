271b57b6bc5b419ed7566353461c3862d443b1c5
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-25 21:28:55 -0300

    feat: controller - books - create, read

--- 03-back-end/06-nodejs-orm-autenticacao/01-orm-interface-do-app-com-bd/02-api-sequelize-books/src/controllers/book.controller.js
+++ 03-back-end/06-nodejs-orm-autenticacao/01-orm-interface-do-app-com-bd/02-api-sequelize-books/src/controllers/book.controller.js
@@ -3,8 +3,33 @@ const BookService = require('../services/book.service');
 const getAll = async (_req, res) => {
   const books = await BookService.getAll();
   res.status(200).json(books)
-}
+};
+
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const { message } = await BookService.getById(id);
+    res.status(200).json(message);
+  } catch (error) {
+    next(error);
+  }
+
+};
+
+const create = async (req, res, next) => {
+
+  try {
+    const { message } = await BookService.create(req.body);
+    res.status(200).json(message);
+  } catch (error) {
+    next(error);
+  }
+
+};
 
 module.exports = {
   getAll,
-}
+  getById,
+  create,
+};
