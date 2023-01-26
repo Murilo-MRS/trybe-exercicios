2eace77d2c5aebfa83b7c14810e62b291eee0879
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-25 21:27:53 -0300

    chore: handle error

--- 03-back-end/06-nodejs-orm-autenticacao/01-orm-interface-do-app-com-bd/02-api-sequelize-books/src/app.js
+++ 03-back-end/06-nodejs-orm-autenticacao/01-orm-interface-do-app-com-bd/02-api-sequelize-books/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const BooksController = require('./controllers/book.controller');
+const handleErrors = require('./middlewares/errors/handleErrors');
+const { booksRouter } = require('./routes');
 const app = express();
 
 app.use(express.json());
 
-app.get('/books', BooksController.getAll)
+app.use('/books', booksRouter);
+
+app.use(handleErrors);
 
 module.exports = app;
-
