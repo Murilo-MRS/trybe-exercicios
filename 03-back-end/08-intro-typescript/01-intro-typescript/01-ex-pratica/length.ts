901608d172f5c40dbb7ede6f492b3b0397501aec
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-03 11:50:00 -0300

    feat: ex 3

--- 03-back-end/08-intro-typescript/01-intro-typescript/01-ex-pratica/length.ts
+++ 03-back-end/08-intro-typescript/01-intro-typescript/01-ex-pratica/length.ts
@@ -1,11 +1,11 @@
 // ./length.ts
 
-const units = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
+const unitsLength = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
 
-function convert(value: number, fromUnit: string, toUnit: string): number {
-  const fromIndex = units.indexOf(fromUnit); // pegamos o index da unidade base no array
-  const toIndex = units.indexOf(toUnit); // pegamos o index da unidade para a conversão
+function convertLength(value: number, fromUnit: string, toUnit: string): number {
+  const fromIndex = unitsMass.indexOf(fromUnit); // pegamos o index da unidade base no array
+  const toIndex = unitsMass.indexOf(toUnit); // pegamos o index da unidade para a conversão
   const exponent = toIndex - fromIndex; // calculamos o expoente a partir da diferença dos index
 
   return value * Math.pow(10, exponent);
-}
+};
