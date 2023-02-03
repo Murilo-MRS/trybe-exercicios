f3a92079d711b99e801eaa48c034f132d2bc76c4
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-03 15:34:20 -0300

    feat: service e controller - book

---
+++ 03-back-end/08-intro-typescript/03-express--com-typescript/express-typescript/controllers/book.controller.ts
@@ -0,0 +1,16 @@
+// ./controllers/books.controller.ts
+
+import { Request, Response } from 'express';
+import statusCodes from '../statusCodes';
+import BookService from '../services/books.service';
+
+class BooksController {
+  constructor(private bookService = new BookService()) { }
+
+  public getAll = async (_req: Request, res: Response) => {
+    const books = await this.bookService.getAll();
+    res.status(statusCodes.OK).json(books);
+  };
+}
+
+export default BooksController;
