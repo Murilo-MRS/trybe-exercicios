a1b7335103a9b900036968b2ca87d2cf6c3fbd88
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-03 15:42:08 -0300

    feat: add endpoint /get byId books

--- 03-back-end/08-intro-typescript/03-express--com-typescript/express-typescript/controllers/book.controller.ts
+++ 03-back-end/08-intro-typescript/03-express--com-typescript/express-typescript/controllers/book.controller.ts
@@ -11,6 +11,19 @@ class BooksController {
     const books = await this.bookService.getAll();
     res.status(statusCodes.OK).json(books);
   };
+
+  public getById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id, 10);
+    const book = await this.bookService.getById(id);
+
+    if (!book) {
+      return res.status(statusCodes.NOT_FOUND)
+        .json({ message: 'Book not found!'});
+    }
+
+    res.status(statusCodes.OK).json(book);
+  }
+
 };
 
 export default BooksController;
