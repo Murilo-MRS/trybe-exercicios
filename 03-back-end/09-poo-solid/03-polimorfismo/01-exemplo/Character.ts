940b87e78d95137ff241458947e189b19f5141b5
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-13 16:33:21 -0300

    feat: 9.3 fixar - metodo estaticos

--- 03-back-end/09-poo-solid/03-polimorfismo/01-exemplo/Character.ts
+++ 03-back-end/09-poo-solid/03-polimorfismo/01-exemplo/Character.ts
@@ -1,42 +1,92 @@
+// abstract class Character {
+//   abstract talk() : void;
+//   abstract specialMove() : void;
+// };
+
+// class MeleeCharacter extends Character {
+//   constructor (private _name : string) {
+//     super();
+//   };
+
+//   talk() : void {
+//     console.log(`Sou ${this._name}`);
+//   };
+
+//   specialMove() : void {
+//     console.log(`${this._name} usou Ataque de perto`);
+//   };
+
+// }
+
+// class LongRangeCharacter extends Character {
+//   constructor (private _name : string) {
+//     super();
+//   };
+
+//   talk() : void {
+//     console.log(`Sou ${this._name}`);
+//   };
+
+//   specialMove() : void {
+//     console.log(`${this._name} usou Ataque de longe`);
+//   };
+
+// }
+
+// const knight = new MeleeCharacter('Yoshi');
+// const archer = new LongRangeCharacter('Samus');
+
+// archer.talk();
+// knight.talk();
+// archer.specialMove();
+// knight.specialMove();
+
 abstract class Character {
-  abstract talk() : void;
-  abstract specialMove() : void;
-};
+  abstract talk(): void;
+  abstract specialMove(): void;
+
+  static characterPresentation(character: Character): void {
+    character.talk();
+    character.specialMove();
+  }
+}
 
 class MeleeCharacter extends Character {
-  constructor (private _name : string) {
+  constructor (private _name : string, private _specialMoveName : string) {
     super();
   };
 
   talk() : void {
-    console.log(`Sou ${this._name}`);
+    console.log(`Sou ${this._name} e ataco de perto`);
   };
 
   specialMove() : void {
-    console.log(`${this._name} usou Ataque de perto`);
+    console.log(`${this._name} usou ${this._specialMoveName}`);
   };
 
 }
 
 class LongRangeCharacter extends Character {
-  constructor (private _name : string) {
+  constructor (private _name : string, private _specialMoveName : string) {
     super();
   };
 
   talk() : void {
-    console.log(`Sou ${this._name}`);
+    console.log(`Sou ${this._name}, e ataco de longe`);
   };
 
   specialMove() : void {
-    console.log(`${this._name} usou Ataque de longe`);
+    console.log(`${this._name} usou ${this._specialMoveName}`);
   };
 
 }
 
-const knight = new MeleeCharacter('Yoshi');
-const archer = new LongRangeCharacter('Samus');
+const yoshi = new MeleeCharacter('Yoshi', 'Punch');
+const samus = new LongRangeCharacter('Samus', 'Laser');
 
-archer.talk();
-knight.talk();
-archer.specialMove();
-knight.specialMove();
+// yoshi.talk();
+// yoshi.specialMove();
+// samus.talk();
+// samus.specialMove();
+Character.characterPresentation(yoshi);
+Character.characterPresentation(samus);
--- 03-back-end/09-poo-solid/03-polimorfismo/01-exemplo/dist/Character.js
+++ 03-back-end/09-poo-solid/03-polimorfismo/01-exemplo/dist/Character.js
@@ -1,40 +1,79 @@
 "use strict";
+// abstract class Character {
+//   abstract talk() : void;
+//   abstract specialMove() : void;
+// };
+// class MeleeCharacter extends Character {
+//   constructor (private _name : string) {
+//     super();
+//   };
+//   talk() : void {
+//     console.log(`Sou ${this._name}`);
+//   };
+//   specialMove() : void {
+//     console.log(`${this._name} usou Ataque de perto`);
+//   };
+// }
+// class LongRangeCharacter extends Character {
+//   constructor (private _name : string) {
+//     super();
+//   };
+//   talk() : void {
+//     console.log(`Sou ${this._name}`);
+//   };
+//   specialMove() : void {
+//     console.log(`${this._name} usou Ataque de longe`);
+//   };
+// }
+// const knight = new MeleeCharacter('Yoshi');
+// const archer = new LongRangeCharacter('Samus');
+// archer.talk();
+// knight.talk();
+// archer.specialMove();
+// knight.specialMove();
 class Character {
+    static characterPresentation(character) {
+        character.talk();
+        character.specialMove();
+    }
 }
-;
 class MeleeCharacter extends Character {
-    constructor(_name) {
+    constructor(_name, _specialMoveName) {
         super();
         this._name = _name;
+        this._specialMoveName = _specialMoveName;
     }
     ;
     talk() {
-        console.log(`Sou ${this._name}`);
+        console.log(`Sou ${this._name} e ataco de perto`);
     }
     ;
     specialMove() {
-        console.log(`${this._name} usou Ataque de perto`);
+        console.log(`${this._name} usou ${this._specialMoveName}`);
     }
     ;
 }
 class LongRangeCharacter extends Character {
-    constructor(_name) {
+    constructor(_name, _specialMoveName) {
         super();
         this._name = _name;
+        this._specialMoveName = _specialMoveName;
     }
     ;
     talk() {
-        console.log(`Sou ${this._name}`);
+        console.log(`Sou ${this._name}, e ataco de longe`);
     }
     ;
     specialMove() {
-        console.log(`${this._name} usou Ataque de longe`);
+        console.log(`${this._name} usou ${this._specialMoveName}`);
     }
     ;
 }
-const knight = new MeleeCharacter('Yoshi');
-const archer = new LongRangeCharacter('Samus');
-archer.talk();
-knight.talk();
-archer.specialMove();
-knight.specialMove();
+const yoshi = new MeleeCharacter('Yoshi', 'Punch');
+const samus = new LongRangeCharacter('Samus', 'Laser');
+// yoshi.talk();
+// yoshi.specialMove();
+// samus.talk();
+// samus.specialMove();
+Character.characterPresentation(yoshi);
+Character.characterPresentation(samus);
