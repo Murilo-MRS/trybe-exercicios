540ef6189016cbfc2d76bc92267e2c4c1fcace22
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-14 09:38:16 -0300

    feat: 9.1 - pratica ex 4

--- 03-back-end/09-poo-solid/01-intro-poo/02-ex-pratica/02-ex.ts
+++ 03-back-end/09-poo-solid/01-intro-poo/02-ex-pratica/02-ex.ts
@@ -112,14 +112,19 @@ class Order {
 
     this._discount = value;
   }
+
+  calculateTotal(): number {
+    return this.items
+      .reduce((previousValue, item) => {
+        const total = previousValue + item.price;
+
+        return total;
+      }, 0);
+  }
+
+  calculateTotalWithDiscount(): number {
+    return this.calculateTotal() * (1 - this.discount);
+  }
+
 }
 
-const client = new Client('João');
-
-const sandwich = new OrderItem('Sanduíche Natural', 5.00);
-const juice = new OrderItem('Suco de Abacaxi', 5.00);
-const dessert = new OrderItem('Gelatina de Uva', 2.50);
-
-const order = new Order(client, [sandwich, juice, dessert], 'dinheiro', 0.10);
-
-console.log(order);
