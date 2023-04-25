74b4413f5065c51e1fb0cdc10a51e6d6ffe4c696
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-25 20:48:24 -0300

    fix: removendo .close()
    não é necessário com with

--- 04-ciencia-da-computacao/01-intro-python/02-entrada-saida-de-dados/01-exemplos/11_exemplo.py
+++ 04-ciencia-da-computacao/01-intro-python/02-entrada-saida-de-dados/01-exemplos/11_exemplo.py
@@ -6,13 +6,11 @@ with open("file.txt") as grades_file:
         if int(student_grade[1]) < 6:
             recovery_students.append(student_grade[0] + "\n")
 print(grades_file.closed)
-grades_file.close()
 
 with open("recuStudents.txt", mode="w") as recu_students_file:
     print(recovery_students)
     recu_students_file.writelines(recovery_students)
 print(recu_students_file.closed)
-recu_students_file.close()
 
 # file.txt
 # Marcos 10
