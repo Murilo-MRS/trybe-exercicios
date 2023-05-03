8644fc08332e9adddf19568fc537e9cf098cad6b
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-03 16:08:08 -0300

    feat: 2.1 - add fixar com classe ventilador

--- 04-ciencia-da-computacao/02-padroes-de-projeto/01-poo-em-python/Lidificador.py
+++ 04-ciencia-da-computacao/02-padroes-de-projeto/01-poo-em-python/Lidificador.py
@@ -43,16 +43,34 @@ class Liquidificador:
         return self.__ligado
 
 
+class Ventilador:
+    def __init__(self, cor, potencia, tensao, preco):
+        self.preco = preco
+        self.__cor = cor
+        self.__potencia = potencia
+        self.__tensao = tensao
+        self.__ligado = False
+
+    def cor(self):
+        return self.__cor
+
+
 class Pessoa:
     def __init__(self, nome, saldo_na_conta):
         self.nome = nome
         self.saldo_na_conta = saldo_na_conta
         self.liquidificador = None
+        self.ventilador = None
 
     def comprar_liquidificador(self, liquidificador):
         if liquidificador.preco <= self.saldo_na_conta:
             self.saldo_na_conta -= liquidificador.preco
             self.liquidificador = liquidificador
+
+    def comprar_ventilador(self, ventilador):
+        if ventilador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= ventilador.preco
+            self.ventilador = ventilador
 
     def __str__(self):
         return (
@@ -77,3 +95,9 @@ print(liquidificador.cor)  # VERMELHO
 pessoa_cozinheira = Pessoa("Jacquin", 1000)
 pessoa_cozinheira.comprar_liquidificador(liquidificador)
 print(pessoa_cozinheira)
+
+ventilador_branco = Ventilador("branco", potencia=250, tensao=220, preco=100)
+pessoa_comum = Pessoa("Maria", saldo_na_conta=2000)
+pessoa_comum.comprar_ventilador(ventilador_branco)
+
+print(pessoa_comum)
