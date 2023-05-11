344697188776056c9b4976c2260d49de8433e297
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-11 18:04:38 -0300

    feat: 3.1 ad ex timeout

---
+++ 04-ciencia-da-computacao/03-raspagem-dados/01-raspagem-de-dados/timeout_exemplo.py
@@ -0,0 +1,13 @@
+import requests
+
+# Por 10 segundos não temos certeza se a requisição irá retornar
+# response = requests.get("https://httpbin.org/delay/10")
+
+try:
+    # recurso demora muito a responder
+    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.ReadTimeout:
+    # vamos fazer uma nova requisição
+    response = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    print(response.status_code)
