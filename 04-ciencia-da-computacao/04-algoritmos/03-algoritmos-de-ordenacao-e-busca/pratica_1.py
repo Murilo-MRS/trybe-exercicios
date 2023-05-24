ea45d9808a4d32204b2ddcf09ff7eeefe66c93e8
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-24 18:07:28 -0300

    feat: 4.2 - add ex 1

---
+++ 04-ciencia-da-computacao/04-algoritmos/03-algoritmos-de-ordenacao-e-busca/pratica_1.py
@@ -0,0 +1,7 @@
+["zebra", "macaco", "elefante", "arara", "javali"]
+
+# 1 iteracoes depois
+["macaco", "elefante", "arara", "javali", "zebra"]
+
+# 2 iteracoes depois
+["elefante", "arara", "javali", "macaco", "zebra"]
