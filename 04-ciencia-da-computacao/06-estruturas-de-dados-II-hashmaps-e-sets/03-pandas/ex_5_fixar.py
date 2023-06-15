690631738591da64892a39113982ee148d8e2394
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-15 17:26:39 -0300

    refact: 6.3 add tratamento de erro

--- 04-ciencia-da-computacao/06-estruturas-de-dados-II-hashmaps-e-sets/03-pandas/ex_5_fixar.py
+++ 04-ciencia-da-computacao/06-estruturas-de-dados-II-hashmaps-e-sets/03-pandas/ex_5_fixar.py
@@ -2,12 +2,20 @@ import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
 
-database_url = "mysql+pymysql://root:admin@localhost:3306/chuvas"
+# usar CREATE DATABASE chuvas; antes no banco de dados
+database_url = "mysql+pymysql://root:password@localhost:3306/chuvas"
 
 engine = create_engine(database_url)
+conn = engine.connect()
 
 chuvas_regiao_morumbi = pd.read_csv("chuvas_morumbi.csv")
 
-chuvas_regiao_morumbi.to_sql(
-    name="chuvas_no_morumbi", con=engine, if_exists="replace"
-)
+try:
+    chuvas_regiao_morumbi.to_sql(
+        name="chuvas_no_morumbi", con=engine, if_exists="replace"
+    )
+
+except Exception as error:
+    print("Algo deu errado", error)
+finally:
+    conn.close()
