b446c040db22ce8bb59c69572d7538208634b167
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-15 17:35:02 -0300

    fix: lint problems

--- 04-ciencia-da-computacao/06-estruturas-de-dados-II-hashmaps-e-sets/03-pandas/ex_5_fixar.py
+++ 04-ciencia-da-computacao/06-estruturas-de-dados-II-hashmaps-e-sets/03-pandas/ex_5_fixar.py
@@ -4,7 +4,7 @@ import pymysql
 
 # usar CREATE DATABASE chuvas; antes no banco de dados
 database_url = "mysql+pymysql://root:password@localhost:3306/chuvas"
-
+print(pymysql)
 engine = create_engine(database_url)
 conn = engine.connect()
 
